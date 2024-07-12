e2b065610c00232f8a1ff48def4efb26e40da528
Kris O'Shea <minimallifter@protonmail.com>
2024-07-12 23:46:06 +0200

    configure foundry repo to support scaffold eth

--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -1,21 +1,37 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
-import {Script, console} from "../lib/forge-std/src/Script.sol";
+import "../contracts/YourContract.sol";
+import "./DeployHelpers.s.sol";
 
-contract DeployScript is Script {
-    function setUp() public {}
+contract DeployScript is ScaffoldETHDeploy {
+  error InvalidPrivateKey(string);
 
-    function run() public {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address deployerAddress = vm.addr(deployerPrivateKey);
-
-        vm.startBroadcast(deployerPrivateKey);
-
-        YourContract yourContract = new YourContract(deployerAddress);
-
-        console.log("YourContract deployed to:", address(yourContract));
-
-        vm.stopBroadcast();
+  function run() external {
+    uint256 deployerPrivateKey = setupLocalhostEnv();
+    if (deployerPrivateKey == 0) {
+      revert InvalidPrivateKey(
+        "You don't have a deployer account. Make sure you have set DEPLOYER_PRIVATE_KEY in .env or use `yarn generate` to generate a new random account"
+      );
     }
+    vm.startBroadcast(deployerPrivateKey);
+
+    YourContract yourContract = new YourContract(vm.addr(deployerPrivateKey));
+    console.logString(
+      string.concat(
+        "YourContract deployed at: ", vm.toString(address(yourContract))
+      )
+    );
+
+    vm.stopBroadcast();
+
+    /**
+     * This function generates the file containing the contracts Abi definitions.
+     * These definitions are used to derive the types needed in the custom scaffold-eth hooks, for example.
+     * This function should be called last.
+     */
+    exportDeployments();
+  }
+
+  function test() public { }
 }
